2f1a1cf1556d38654aeed0e58f10cb2eb293dd56
ralfguv <123738522+ralfguv@users.noreply.github.com>
2024-02-13 11:31:06 -0300

    ajuste

---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+const   sidebar = document.querySelector("sidebar"),
+        main = document.querySelector("main"),
+        toogle = document.querySelector(".toogle"),
+        textlink = document.querySelector(".textlink");
+
+
+        toogle.addEventListener("click", () => {
+            sidebar.style.width = '50px'
+        });
+        
+      
