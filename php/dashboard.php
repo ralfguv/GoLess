363f3e61941a881ef34b4c5decdfca4bdaffbca2
ralfguv <123738522+ralfguv@users.noreply.github.com>
2024-02-14 17:05:20 -0300

    teste

--- php/dashboard.php
+++ php/dashboard.php
@@ -375,7 +375,7 @@ if (!empty($_GET['id'])) {
                                     <td>R$ 300,00</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">6</th>
+                                    <th scope="row">7</th>
                                     <td>Amazon</td>
                                     <td>10</td>
                                     <td>R$ 300,00</td>
@@ -386,7 +386,34 @@ if (!empty($_GET['id'])) {
 
 
                     <div class="Geral grafico-estilo">
-                        <p>Noticias GoLess</p>
+
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner size-image-propaganda">
+                                <div class="carousel-item active">
+                                    <img src="../imagens/Elementos_Gráficos/Hub/Dashboard/banner1.png" class="d-block w-100" alt="Propaganda">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../imagens/Elementos_Gráficos/Hub/Dashboard/Banner2.jpg" class="d-block w-100" alt="Propaganda">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="../imagens/Elementos_Gráficos/Hub/Dashboard/Banner3.png" class="d-block w-100" alt="Propaganda">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
         </main>
